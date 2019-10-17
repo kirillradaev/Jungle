7a5b1baf52cb13e6a882d949ad1273f14e99c1e5
kirillradaev <cyrilrdv@gmail.com>
2019-10-17 02:19:54 +0000

    order details displayed

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,10 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  # def total
+  #   @total = ((line_items.inject(0) {|sum, li| sum + (li.quantity * li.total_price_cents) }) / 100).round(2)
+  # end
+
+  
+
 end
