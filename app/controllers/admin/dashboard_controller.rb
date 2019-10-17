02b9517af711d09bb19a8308855aac82d7f209db
kirillradaev <cyrilrdv@gmail.com>
2019-10-17 19:23:31 +0000

    dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
   password: ENV["HTTP_BASIC_PASSWORD"],
   if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
-  
+
   def show
+    @number_of_products = Product.count
+    @number_of_categories = Category.count
   end
 end
